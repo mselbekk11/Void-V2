d3ff1d1729b039516d146a7821dfd1432aeaf3fc
Morgan Selbekk <mselbekk11@gmail.com>
2021-12-25 15:00:33 +0000

    First upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from "react";
+import './App.css';
+import Void from './components/void/void';
+
+
+
+function App() {
+  return (
+    <div className="App">
+      <Void/>
+    </div>
+  );
+}
+
+export default App;
+
+
+// function App() {
+//   return (
+//       <div className="App">
+//         <NavBar />
+//         <Link to="/invoices">Invoices</Link> |{" "}
+//         <Link to="/expenses">Expenses</Link>
+//       </div>
+//   );
+// }
+
+// export default App;
+
+
+
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Bookkeeper</h1>
+//       <nav
+//         style={{
+//           borderBottom: "solid 1px",
+//           paddingBottom: "1rem"
+//         }}
+//       >
+//         <Link to="/Home">Home</Link> |{" "}
+//         <Link to="/Constellation">CourseOne</Link>
+//       </nav>
+//     </div>
+//   );
+// }
+
